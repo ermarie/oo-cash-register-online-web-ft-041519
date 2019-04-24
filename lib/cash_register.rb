65e69a9a41ff510115b11132d217092a27612a65
Erin Stalzer <erinstalzer@gmail.com>
2019-04-24 18:01:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
   attr_accessor :item, :quantity, :price, :total, :discount
 
-  def initialize(*discount)
+  def initialize(*discount = 0)
     @total = 0
     puts "discount: #{discount}"
     @discount = discount
